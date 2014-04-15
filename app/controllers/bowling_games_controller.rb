59940f8620fcd826eccd0eded16590dcd60adb14
Joel Turnbull <joelbywan@gmail.com>
2014-04-15 09:58:05 -0400

    initial

--- app/controllers/bowling_games_controller.rb
+++ app/controllers/bowling_games_controller.rb
@@ -1,12 +1,12 @@
 class BowlingGamesController < ApplicationController
   def edit
-    binding.pry
     @bowling_game = BowlingGame.find(params[:id])
   end
 
   def update
     bowling_game = BowlingGame.find(params[:id])
-    Throw.create(pins:params[:pins],bowling_game:bowling_game)
+    pins = params[:pins]
+    Throw.create(pins:pins, bowling_game:bowling_game)
     redirect_to edit_bowling_game_path(bowling_game)
   end
 end
--- app/models/bowling_game.rb
+++ app/models/bowling_game.rb
@@ -2,7 +2,6 @@ class BowlingGame < ActiveRecord::Base
   has_many :throws
 
   def frames
-    binding.pry
     throws.each_slice(2) 
   end
 end
