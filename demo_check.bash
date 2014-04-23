a49ab311506032834ce2ad758f8323e1a4d16f5f
Joel Turnbull <joelbywan@gmail.com>
2014-04-23 18:31:42 -0500

    add demo preparer

---
+++ demo_check.bash
@@ -0,0 +1,6 @@
+  git co master
+  git co .
+  git co $DEMO1
+  rake db:seed
+  vi -o app/controllers/bowling_games_controller.rb app/models/bowling_game.rb app/views/bowling_games/edit.html.erb
+# open all files 
